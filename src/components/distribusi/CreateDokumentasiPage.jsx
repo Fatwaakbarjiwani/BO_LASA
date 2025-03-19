e56050f78e9d4b5098292bd76ce46dfa96f4f30a
Fatwaakbarjiwani <fatwaakbar2003@gmail.com>
2025-03-20 03:07:39 +0700

    update

--- src/components/distribusi/CreateDokumentasiPage.jsx
+++ src/components/distribusi/CreateDokumentasiPage.jsx
@@ -367,39 +367,37 @@ function CreateDokumentasiPage() {
               Deskripsi
             </label>
             <Editor
-              apiKey="e3m2xvaijq07t0pv56vwlvxn41f4ro15a65t7kbjpt9packt"
+              apiKey="e3m2xvaijq07t0pv56vwlvxn41f4ro15a65t7kbjpt9packt" // Ganti dengan API key Anda atau hapus untuk mode testing
               value={description}
               onEditorChange={(newValue) => setDescription(newValue)}
               init={{
                 branding: false,
+                height: 400,
+                menubar: true,
                 plugins: [
-                  // Core editing features
-                  "anchor",
+                  "advlist",
                   "autolink",
-                  "charmap",
-                  "codesample",
-                  "emoticons",
-                  "image",
-                  "link",
                   "lists",
-                  "media",
+                  "link",
+                  "image",
+                  "charmap",
+                  "preview",
+                  "anchor",
                   "searchreplace",
-                  "table",
                   "visualblocks",
+                  "code",
+                  "fullscreen",
+                  "insertdatetime",
+                  "media",
+                  "table",
                   "wordcount",
                 ],
                 toolbar:
-                  "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
-                tinycomments_mode: "embedded",
-                tinycomments_author: "Author name",
-                mergetags_list: [
-                  { value: "First.Name", title: "First Name" },
-                  { value: "Email", title: "Email" },
-                ],
-                ai_request: (request, respondWith) =>
-                  respondWith.string(() =>
-                    Promise.reject("See docs to implement AI Assistant")
-                  ),
+                  "undo redo | formatselect | bold italic underline | " +
+                  "alignleft aligncenter alignright alignjustify | " +
+                  "bullist numlist outdent indent | link image media | removeformat",
+                content_style:
+                  "body { font-family:Arial,Helvetica,sans-serif; font-size:14px }",
               }}
             />
           </div>
